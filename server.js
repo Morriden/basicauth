dd5cce273904ae2d03aaa4902eeace4db0844e48
Erik Ford <ekford18@gmail.com>
2020-06-15 17:31:22 -0700

    completed lab except for events closing

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+setInterval(() => {
+  
+}, 1000 * 60 * 60);
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
